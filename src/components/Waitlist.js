dc041b86eabe6b3b0478879dd8f23e6990771297
Andy Ly <116235443+aICE-eth@users.noreply.github.com>
2023-06-17 21:05:40 -0700

    Add files via upload

--- src/components/Waitlist.js
+++ src/components/Waitlist.js
@@ -1,64 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import LogoTwo from '../images/Group 49 (2).svg'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 
-const Waitlist = ({onNavigate}) => {
+
+const Waitlist = () => {
   const [email, setEmail] = useState('');
   const [waitlist, setWaitlist] = useState([]);
+  const [showInput, setShowInput] = useState(true);
+  const [showThankYou, setShowThankYou] = useState(false);
 
   useEffect(() => {
     const storedWaitlist = localStorage.getItem('waitlist');
     if (storedWaitlist) {
       setWaitlist(JSON.parse(storedWaitlist));
     }
-  },[]);
+  }, []);
+
 
   const handleInputChange = (e) => {
     setEmail(e.target.value);
   };
 
-  const handleAddToWaitlist = () => {
-    if (email.trim() !== '') {
-      const updatedWaitlist = [...waitlist, email];
-      setWaitlist(updatedWaitlist);
-      setEmail('');
-      localStorage.setItem('waitlist', JSON.stringify(updatedWaitlist));
-    }
+  const handleSaveClick = () => {
+    setWaitlist([...waitlist, email]);
+    setEmail('');
+    localStorage.setItem('waitlist', JSON.stringify(waitlist));
+    setShowInput(false);
+    setShowThankYou(true);
   };
+
+  const handleGoBack = () => {
+    setShowInput(true);
+    setShowThankYou(false);
+  };
+
 
   return (
     <div>
       
       <ul className = 'nav'>
+    
         <li><img className = 'logoAlt'src ={LogoTwo} /></li>
         <li className='company-name'>
           <h1 className='course'>Course</h1>
-          <h1 className='purple'>Cube</h1>
+          <h1 className='purple'>Cubes</h1>
         </li>
         <li className = 'aboutLi'><button className='About'><h2 className = 'h2About'>About Us</h2></button></li>
       </ul>  
-      <h2 className='waitlist'>Waitlist</h2>
-      <input className='input' placeholder='Your Email' type="email" value={email} onChange={handleInputChange} />
-      <button className='save' onClick={handleAddToWaitlist}>Save</button>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Email</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {waitlist.map((email, index) => (
-              <TableRow key={index}>
-                <TableCell>{email}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      <button onClick={()=>onNavigate}> GOOGOGOGOGO</button>
+        <div className ='texts'>
+          <p className='waitlist'>TikTok for Education! </p>
+        </div>
+      {showInput && (
+        <div>
+          <input className='input' placeholder='Enter Your Email' type="email" value={email} onChange={handleInputChange}  />
+          <button className='save' onClick={handleSaveClick}>Join Waitlist</button>
+        </div>
+        )} 
+      {showThankYou && (
+        <div>
+          <button onClick={handleGoBack} className ='thankBtn'> <p className='thank-you'>Thank you!</p></button>
+        </div>
+      )}
     </div>
   );
 };
